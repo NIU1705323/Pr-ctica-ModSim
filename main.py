ff6237f8760fa1b1a9453dc58a6186bdce41fa74
NIU1705323 <1705323@uab.cat>
2026-06-14 18:49:28 +0200

    Optimitzacions

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from utils.utils import *
 from utils.moviment_agent import moure_agent
 from config import *
 from random import seed
+from time import time
 
 if __name__ == "__main__":
     
     seed(SEED) # Assignem una inicialització concreta si està definida
+    temps = time()
     t=list(range(TIPUS_DE_VEINS))+[-1] # Valors de les diferents caselles
 
     p=PROBABILITATS_VEINS+[1-sum(PROBABILITATS_VEINS)] # Probabilitats de cada tipus
@@ -26,6 +28,7 @@ if __name__ == "__main__":
     
     # SIMULACIÓ
     canvis = -1 # Aquest valor està per a evitar errors
+    buides = np.argwhere(arr == -1) # Totes les posicions ocupades
     segregacio = []
     for iteracio in range(MAX_ITER):
         valor = segregacio_local(arr)
@@ -36,14 +39,16 @@ if __name__ == "__main__":
 
         # Movem els veins que es volen moure
         for posicio in posicions:
-            if satisfet(posicio, arr) < TAU:
-                if moure_agent(posicio, arr) == True: canvis += 1
+            satisfaccio = satisfet(posicio, arr)
+            if satisfaccio < TAU:
+                if moure_agent(posicio, arr, buides, satisfaccio) == True: canvis += 1
 
         # Mostrar evolució cada X visualitzacions
         if not iteracio%VISUALITZACIONS and imatge is not None: dibuixar(imatge, iteracio+1, arr)
 
         # Mostrem dades com poden ser el numero de moviments
-        print(f"Iteració {iteracio + 1}: {canvis} moviments,{' '*(7-len(str(canvis)))}segregació: {segregacio[-1]}")
+        print(f"Iteració {str(iteracio + 1).rjust(2)}: {str(canvis).rjust(3)} moviments,"
+              f" segregació: {segregacio[-1]:.5f}, temps actual: {(time() - temps):.5f}s")
 
         # Equilibri
         if canvis == 0:
@@ -57,6 +62,7 @@ if __name__ == "__main__":
     # ESTAT FINAL
     print("\nESTAT FINAL:\n")
     print(arr)
+    print(time() - temps, "segons")
 
     if canvis != 0 and imatge is not None: imatge["ax"].set_title(f"Equilibri NO assolit en l'iteració {MAX_ITER}")
     plt.show()
--- utils/moviment_agent.py
+++ utils/moviment_agent.py
@@ -12,24 +12,26 @@ import numpy as np
 from config import *
 from utils.utils import *
 
-def moure_agent(posicio: np.ndarray, mapa: np.ndarray) -> bool:
+def moure_agent(posicio: np.ndarray, mapa: np.ndarray, 
+                buides: np.ndarray, satisfaccio: float) -> bool:
     """Funció que desplaça a l'individu de la posició argument a una millor"""
     match MOVIMENT:
-        case 0: return moure_agent_adjacent(posicio, mapa)
-        case 1: return moure_agent_random(posicio, mapa)
-        case 2: return moure_agent_global(posicio, mapa)
-        case 3: return moure_agent_primera_millor(posicio, mapa)
+        case 0: return moure_agent_adjacent(posicio, mapa, buides, satisfaccio)
+        case 1: return moure_agent_random(posicio, mapa, buides, satisfaccio)
+        case 2: return moure_agent_global(posicio, mapa, buides, satisfaccio)
+        case 3: return moure_agent_primera_millor(posicio, mapa, buides, satisfaccio)
     return False # Per evitar errors
 
 
-def moure_agent_adjacent(posicio: np.ndarray, mapa: np.ndarray) -> bool:
+def moure_agent_adjacent(posicio: np.ndarray, mapa: np.ndarray, 
+                         buides: np.ndarray, satisfaccio: float) -> bool:
     """Funció que desplaça a l'individu a una posició 'adjacent' millor"""
     
     # Definim les variables que necessitem
     tipus = mapa[tuple(posicio)]
     if tipus == -1: return False # Per a evitar errors
     nova_pos = None
-    bst_prop = satisfet(posicio, mapa, clase=tipus)
+    bst_prop = satisfaccio
     if bst_prop >= SATISFET: return False # Per a evitar errors
     indexos_veins = INDEXOS_VEINS.copy()
     np.random.shuffle(indexos_veins) # les revisem en ordre aleatori
@@ -38,10 +40,11 @@ def moure_agent_adjacent(posicio: np.ndarray, mapa: np.ndarray) -> bool:
     for pos in indexos_veins:
         vei = np.array(posicio) + np.array(pos)
         if index_valid(vei, mapa) and mapa[tuple(vei)] == -1:
-            mapa_temp = mapa.copy()
-            mapa_temp[tuple(vei)] = tipus
-            mapa_temp[tuple(posicio)] = -1
-            prop = satisfet(vei, mapa_temp, clase=tipus) # Revisem si estaria satisfet
+            mapa[tuple(vei)] = tipus
+            mapa[tuple(posicio)] = -1
+            prop = satisfet(vei, mapa, clase=tipus) # Revisem si estaria satisfet
+            mapa[tuple(posicio)] = tipus
+            mapa[tuple(vei)] = -1
             if prop >= SATISFET: # En cas afirmatiu, ens movem a aquella posició
                 nova_pos = vei
                 break
@@ -56,79 +59,95 @@ def moure_agent_adjacent(posicio: np.ndarray, mapa: np.ndarray) -> bool:
     return True
 
 
-def moure_agent_random(posicio: np.ndarray, mapa: np.ndarray) -> bool:
+def moure_agent_random(posicio: np.ndarray, mapa: np.ndarray, 
+                       buides: np.ndarray, satisfaccio: float) -> bool:
     """Funció que desplaça a l'individu a una posició alèatoria"""
-    
-    buides = np.argwhere(mapa == -1) # busquem caselles buides O(n^^4) :(
 
     if len(buides) == 0: return False
-
-    nova_pos = buides[np.random.randint(len(buides))] # nova posició aleatoria (el agent utilitza la porta mágica)
+    idx_nova_pos = np.random.randint(len(buides))
+    nova_pos = buides[idx_nova_pos] # nova posició aleatoria (el agent utilitza la porta mágica)
     mapa[tuple(nova_pos)] = mapa[tuple(posicio)]
     mapa[tuple(posicio)] = -1
+    buides[idx_nova_pos] = posicio.copy()
     return True
 
-
-def moure_agent_global(posicio: np.ndarray, mapa: np.ndarray) -> bool:
+def moure_agent_global(posicio: np.ndarray, mapa: np.ndarray, 
+                       buides: np.ndarray, satisfaccio: float) -> bool:
     """Funció que desplaça a l'individu a la millor posició del mapa"""
     
     # Definim les variables que necessitem
-    buides = np.argwhere(mapa == -1) # busquem caselles buides
     if len(buides) == 0: return False
-    np.random.shuffle(buides) # les revisem en ordre aleatori
+    
+    # Crear índexs per a poder rastrejar les posicions i fer shuffle
+    indexs = np.arange(len(buides))
+    np.random.shuffle(indexs) # les revisem en ordre aleatori
     
     tipus = mapa[tuple(posicio)]
     if tipus == -1: return False # Per a evitar errors
     nova_pos = None
-    bst_prop = satisfet(posicio, mapa, clase=tipus)
+    idx_nova_pos = None
+    bst_prop = satisfaccio
     if bst_prop >= SATISFET: return False # Per a evitar errors
     
-    # Busquem caselles buides adjacents
-    for vei in buides:
-        mapa_temp = mapa.copy()
-        mapa_temp[tuple(vei)] = tipus
-        mapa_temp[tuple(posicio)] = -1
-        prop = satisfet(vei, mapa_temp, clase=tipus) # Revisem si estaria satisfet
+    # Busquem caselles buides
+    for idx in indexs:
+        vei = buides[idx]
+        mapa[tuple(vei)] = tipus
+        mapa[tuple(posicio)] = -1
+        prop = satisfet(vei, mapa, clase=tipus) # Revisem si estaria satisfet
+        mapa[tuple(posicio)] = tipus
+        mapa[tuple(vei)] = -1
         if prop >= SATISFET: # En cas afirmatiu, ens movem a aquella posició
             nova_pos = vei
+            idx_nova_pos = idx
             break
         if prop > bst_prop: # En cas negatiu, guardem la millor
             bst_prop = prop
             nova_pos = vei
+            idx_nova_pos = idx
 
     # Si no trobem cap casella on estaria satisfet, anem a una qualsevol
     if nova_pos is None: return False
     mapa[tuple(nova_pos)] = tipus
     mapa[tuple(posicio)] = -1
+    buides[idx_nova_pos] = posicio.copy()
     return True
 
 
-def moure_agent_primera_millor(posicio: np.ndarray, mapa: np.ndarray) -> bool:
+def moure_agent_primera_millor(posicio: np.ndarray, mapa: np.ndarray, 
+                               buides: np.ndarray, satisfaccio: float) -> bool:
     """Funció que desplaça a l'individu a qualsevol millor posició del mapa"""
     
     # Definim les variables que necessitem
-    buides = np.argwhere(mapa == -1) # busquem caselles buides
     if len(buides) == 0: return False
-    np.random.shuffle(buides) # les revisem en ordre aleatori
+    
+    # Crear índexs per a poder rastrejar les posicions i fer shuffle
+    indexs = np.arange(len(buides))
+    np.random.shuffle(indexs) # les revisem en ordre aleatori
     
     tipus = mapa[tuple(posicio)]
     if tipus == -1: return False # Per a evitar errors
     nova_pos = None
-    bst_prop = satisfet(posicio, mapa, clase=tipus)
+    idx_nova_pos = None
+    bst_prop = satisfaccio
     if bst_prop >= SATISFET: return False # Per a evitar errors
     
-    # Busquem caselles buides adjacents
-    for vei in buides:
-        mapa_temp = mapa.copy()
-        mapa_temp[tuple(vei)] = tipus
-        mapa_temp[tuple(posicio)] = -1
-        prop = satisfet(vei, mapa_temp, clase=tipus) # Revisem si estaria satisfet
+    # Busquem caselles buides
+    for idx in indexs:
+        vei = buides[idx]
+        mapa[tuple(vei)] = tipus
+        mapa[tuple(posicio)] = -1
+        prop = satisfet(vei, mapa, clase=tipus) # Revisem si estaria satisfet
+        mapa[tuple(posicio)] = tipus
+        mapa[tuple(vei)] = -1
         if prop >= bst_prop: # En cas afirmatiu, ens movem a aquella posició
             nova_pos = vei
+            idx_nova_pos = idx
             break
 
     # Si no trobem cap casella on estaria satisfet, anem a una qualsevol
     if nova_pos is None: return False
     mapa[tuple(nova_pos)] = tipus
     mapa[tuple(posicio)] = -1
+    buides[idx_nova_pos] = posicio.copy()
     return True
